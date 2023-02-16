f19ad31bf99efe2e3b1fd9880e69bf90dd912ee5
Ruslan Mordovanets <gifler@me.com>
2023-02-17 00:52:06 +0200

    first commit v.06

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -54,6 +54,8 @@ const Form = () => {
     return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
+
+          
            <input
                 className={'input'}
                 type="text"
