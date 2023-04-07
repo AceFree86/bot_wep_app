a7f29fbf018e65b56794678b6ab471a02a59a24f
Ruslan Mordovanets <gifler@me.com>
2023-04-07 23:46:17 +0300

    update code libery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useTelegram } from './hooks/useTelegram';
 import Perechin from './components/Perechin/Perechin';
 import Uzhhorod from './components/Uzhhorod/Uzhhorod';
 import Appellate from './components/Appellate/Appellate';
