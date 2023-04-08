6c43d71845d547a4b974e9e5f36b91a748f67b5b
Ruslan Mordovanets <gifler@me.com>
2023-04-08 22:26:49 +0300

    add Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,44 +2,72 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import { Form } from "react-bootstrap";
 
-
 const Register = () => {
-    const [input_text, setText] = useState("");
-    const { tg } = useTelegram();
+  const [input_text, setText] = useState("");
+  const { tg } = useTelegram();
 
-    useEffect(() => {
+  const onSendData = useCallback(() => {
+    const data = { input_text };
+    tg.sendData(JSON.stringify(data));
+  }, [input_text]);
+
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", onSendData);
+    console.log(tg.initData?.user?.id);
+    console.log(tg.initData?.user?.username);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
+  }, [onSendData]);
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  useEffect(() => {
     tg.expand();
     tg.MainButton.setParams({ text: "Записати" });
     tg.MainButton.show();
   }, []);
 
-
-    const onTextChange = (e) => {
-        const value = e.target.value;
-        setText(value);
+  useEffect(() => {
+    if (!input_text) {
+      tg.MainButton.disable();
+    } else {
+      tg.MainButton.enable();
     }
+  }, [input_text]);
 
 
-    return (
-      <div className={"form"}>
-        <h5>Реєстрація для сповіщення</h5>
-        <Form.Control
-          size="lg"
-          type="text"
-          placeholder="Пропишіть ПІП або номер справи"
-          value={input_text}
-          onChange={onTextChange}
-          className={"selectF"}
-        />
-        <Form.Text className="text-muted">
-          наберіть⌨ прізвище та ім'я або номер справи і відправте📥.
-          <b>Попередження</b>❗ Прізвище та ім'я набирати можна з малої букви,
-          а апостроф в тексті " "заміняється на пробіл. Щодо номера справи
-          набираємо⌨ відповідно до зразків " "304/555/20 або 555/20. Інформацію
-          вказуйте правильно💯.
-        </Form.Text>
-      </div>
-    );
+  const onTextChange = (e) => {
+    const value = e.target.value;
+    setText(value);
+  };
+
+  return (
+    <div className={"form"}>
+      <h5>Реєстрація для сповіщення</h5>
+      <Form.Control
+        size="lg"
+        type="text"
+        placeholder="Пропишіть ПІП або номер справи"
+        value={input_text}
+        onChange={onTextChange}
+        className={"selectF"}
+      />
+      <Form.Text className="text-muted">
+        {tg.initData.user.username && (
+          <>
+            {tg.initData?.user?.username} наберіть⌨ прізвище та ім'я або номер
+            справи і ✍🏻записати. <b>Попередження</b>❗ Прізвище та ім'я набирати
+            можна з малої букви, а апостроф в тексті " "заміняється на пробіл.
+            Щодо номера справи набираємо⌨ відповідно до зразків " "304/555/20
+            або 555/20. Інформацію вказуйте правильно💯.
+          </>
+        )}
+      </Form.Text>
+    </div>
+  );
 };
 
 export default Register;
