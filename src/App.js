3686b103e0264f2e6ccf62570dcd10b680c69af9
Ruslan Mordovanets <gifler@me.com>
2023-04-15 11:22:47 +0300

    refacter   tg.expand();

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import { Route, Routes } from 'react-router-dom';
-import Perechin from './components/Perechin/Perechin';
-import Uzhhorod from './components/Uzhhorod/Uzhhorod';
-import Appellate from './components/Appellate/Appellate';
-import GreatBerezny from './components/GreatBerezny/GreatBerezny';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import { Route, Routes } from "react-router-dom";
+import Perechin from "./components/Perechin/Perechin";
+import Uzhhorod from "./components/Uzhhorod/Uzhhorod";
+import Appellate from "./components/Appellate/Appellate";
+import GreatBerezny from "./components/GreatBerezny/GreatBerezny";
 
 function App() {
+  const { tg } = useTelegram();
 
-  const {tg} = useTelegram();
-
-   useEffect(() => {
-        tg.ready();
-    }, [])
+  useEffect(() => {
+    tg.expand();
+    tg.ready();
+  }, []);
 
   return (
     <div className="App">
-        <Routes>
-          <Route index element={<Perechin />}/>
-          <Route path={'uzhhorod'} element={<Uzhhorod />}/>
-          <Route path={'appellate'} element={<Appellate />}/>
-          <Route path={'greatberezny'} element={<GreatBerezny />}/>
-        </Routes>
+      <Routes>
+        <Route index element={<Perechin />} />
+        <Route path={"uzhhorod"} element={<Uzhhorod />} />
+        <Route path={"appellate"} element={<Appellate />} />
+        <Route path={"greatberezny"} element={<GreatBerezny />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Perechin/Perechin.jsx
+++ src/components/Perechin/Perechin.jsx
@@ -3,122 +3,121 @@ import { useTelegram } from "../../hooks/useTelegram";
 import { Form } from "react-bootstrap";
 
 const Perechin = () => {
-    const [dob, setDob] = useState(new Date().toISOString().slice(0, 10));
-    const [input_text, setText] = useState("");
-    const [formset, setFormset] = useState("");
-    const [court, setCourt] = useState("");
-    const { tg } = useTelegram();
+  const [dob, setDob] = useState(new Date().toISOString().slice(0, 10));
+  const [input_text, setText] = useState("");
+  const [formset, setFormset] = useState("");
+  const [court, setCourt] = useState("");
+  const { tg } = useTelegram();
 
-    const onSendData = useCallback(() => {
-        const data = { dob, formset, court };
-        tg.sendData(JSON.stringify(data));
-    }, [dob, formset, court]);
+  const onSendData = useCallback(() => {
+    const data = { dob, formset, court };
+    tg.sendData(JSON.stringify(data));
+  }, [dob, formset, court]);
 
-    useEffect(() => {
-        tg.onEvent("mainButtonClicked", onSendData);
-        return () => {
-            tg.offEvent("mainButtonClicked", onSendData);
-        };
-    }, [onSendData]);
-
-    useEffect(() => {
-      tg.expand();
-      tg.MainButton.setParams({ text: "Пошук" });
-      tg.MainButton.show();
-    }, []);
-
-
-    const onChangeFormset = (e) => {
-        setFormset(e.target.value);
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
     };
+  }, [onSendData]);
 
-    const onChangeCourt = (e) => {
-        setCourt(e.target.value);
-    };
+  useEffect(() => {
+  
+    tg.MainButton.setParams({ text: "Пошук" });
+    tg.MainButton.show();
+  }, []);
 
-    const onDateCourt = (e) => {
-        setDob(e.target.value);
-    };
+  const onChangeFormset = (e) => {
+    setFormset(e.target.value);
+  };
 
-    const onTextChange = (e) => {
-        const value = e.target.value;
-        setText(value);
-        if (value.trim()) {
-            setDob(value);
-        } else {
-            setDob(new Date().toISOString().slice(0, 10));
-        }
-    };
+  const onChangeCourt = (e) => {
+    setCourt(e.target.value);
+  };
 
-    const options = [
-        { value: "Форма судочинства", label: "Форма судочинства" },
-        {
-            value: "Адміністративні правопорушення",
-            label: "Адміністративні правопорушення",
-        },
-        { value: "Кримінальне судочинство", label: "Кримінальне судочинство" },
-        { value: "Цивільне судочинство", label: "Цивільне судочинство" },
-    ];
+  const onDateCourt = (e) => {
+    setDob(e.target.value);
+  };
 
-    const judges = [
-        { value: "Склад суду", label: "Склад суду" },
-        { value: "Ганько І.І.", label: "Ганько І.І." },
-        { value: "Гевці В.М.", label: "Гевці В.М." },
-        { value: "Чепурнов В.О.", label: "Чепурнов В.О." },
-    ];
+  const onTextChange = (e) => {
+    const value = e.target.value;
+    setText(value);
+    if (value.trim()) {
+      setDob(value);
+    } else {
+      setDob(new Date().toISOString().slice(0, 10));
+    }
+  };
 
-    return (
-      <div className={"form"}>
-        <h5>Перечинський районний суд</h5>
-        <Form.Select
-          aria-label="Floating label select example"
-          size="lg"
-          as="select"
-          value={court}
-          onChange={onChangeCourt}
-          className={"selectF"}
-        >
-          {judges.map((judge) => (
-            <option key={judge.value} value={judge.value}>
-              {" "}
-              {judge.label}{" "}
-            </option>
-          ))}
-        </Form.Select>
-        <Form.Select
-          aria-label="Floating label select example"
-          size="lg"
-          as="select"
-          value={formset}
-          onChange={onChangeFormset}
-          className={"selectF"}
-        >
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </Form.Select>
-        <Form.Control
-          size="lg"
-          type="date"
-          name="dob"
-          placeholder="Due date"
-          value={dob}
-          onChange={onDateCourt}
-          disabled={input_text.trim()}
-          className={"selectF"}
-        />
-        <Form.Control
-          size="lg"
-          type="text"
-          placeholder="Пошук за ПІП або номер справи"
-          value={input_text}
-          onChange={onTextChange}
-          className={"selectF"}
-        />
-      </div>
-    );
+  const options = [
+    { value: "Форма судочинства", label: "Форма судочинства" },
+    {
+      value: "Адміністративні правопорушення",
+      label: "Адміністративні правопорушення",
+    },
+    { value: "Кримінальне судочинство", label: "Кримінальне судочинство" },
+    { value: "Цивільне судочинство", label: "Цивільне судочинство" },
+  ];
+
+  const judges = [
+    { value: "Склад суду", label: "Склад суду" },
+    { value: "Ганько І.І.", label: "Ганько І.І." },
+    { value: "Гевці В.М.", label: "Гевці В.М." },
+    { value: "Чепурнов В.О.", label: "Чепурнов В.О." },
+  ];
+
+  return (
+    <div className={"form"}>
+      <h5>Перечинський районний суд</h5>
+      <Form.Select
+        aria-label="Floating label select example"
+        size="lg"
+        as="select"
+        value={court}
+        onChange={onChangeCourt}
+        className={"selectF"}
+      >
+        {judges.map((judge) => (
+          <option key={judge.value} value={judge.value}>
+            {" "}
+            {judge.label}{" "}
+          </option>
+        ))}
+      </Form.Select>
+      <Form.Select
+        aria-label="Floating label select example"
+        size="lg"
+        as="select"
+        value={formset}
+        onChange={onChangeFormset}
+        className={"selectF"}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Form.Select>
+      <Form.Control
+        size="lg"
+        type="date"
+        name="dob"
+        placeholder="Due date"
+        value={dob}
+        onChange={onDateCourt}
+        disabled={input_text.trim()}
+        className={"selectF"}
+      />
+      <Form.Control
+        size="lg"
+        type="text"
+        placeholder="Пошук за ПІП або номер справи"
+        value={input_text}
+        onChange={onTextChange}
+        className={"selectF"}
+      />
+    </div>
+  );
 };
 
 export default Perechin;
